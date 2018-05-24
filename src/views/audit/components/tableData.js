5c656300165c01f1d534d83706d5a83c6c90e29b
liumapp <liumapp.com@gmail.com>
2018-05-24 15:18:31 +0800

    get detail info

--- src/views/audit/components/tableData.js
+++ src/views/audit/components/tableData.js
@@ -1,3 +1,6 @@
+import { Modal } from 'iview';
+import util from '@/libs/util.js';
+
 export const personalTableColumn = [
     {
         title: '序号',
@@ -45,28 +48,43 @@ export const personalTableColumn = [
         render: (h, params) => {
             const row = params.row;
             const results = [];
-            const view = h('button', {
+            const view = h('Button', {
                 props: {
+                    type: 'text',
                     color: 'blue'
                 },
                 on: {
                     click: () => {
-
+                        util.post('personal-api/api/personal/admincheck/getonedet', {id: row.id}).then(res => {
+                            let content = '';
+                            content += '真实姓名: ' + `${res.data.content.realname}` + '<br>';
+                            content += '身份证号码: ' + `${res.data.content.identitycode}` + '<br>';
+                            content += '身份证有效期: ' + `${res.data.content.identityvalidity}` + '<br>';
+                            content += '手持身份证图片: ' + `<img src="${res.data.content.handidentityphoto}" width="150"/>` + '<br>';
+                            content += '手持身份证正面图片: ' + `<img src="${res.data.content.identityphotofront}" width="150"/>` + '<br>';
+                            content += '手持身份证背面图片: ' + `<img src="${res.data.content.identityphotoreverside}" width="150"/>` + '<br>';
+                            Modal.info({
+                                title: '用户详情',
+                                content: content
+                            });
+                        });
                     }
                 }
             }, '查看');
-            const access = h('button', {
+            const access = h('Button', {
                 props: {
+                    type: 'text',
                     color: 'green'
                 },
                 on: {
                     click: () => {
-
+                        
                     }
                 }
             }, '通过');
-            const refuse = h('button', {
+            const refuse = h('Button', {
                 props: {
+                    type: 'text',
                     color: 'red'
                 },
                 on: {
