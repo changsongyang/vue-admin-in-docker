89d1b181577157d6d481a4d7c3df5665d5c1df14
liumapp <liumapp.com@gmail.com>
2018-05-25 16:57:58 +0800

    ready to add package-order

--- src/views/order/components/tableData.js
+++ src/views/order/components/tableData.js
@@ -1,11 +1,7 @@
 /**
- * @file package-order.vue
+ * @file tableData.js
  * @author liumapp
  * @email liumapp.com@gmail.com
  * @homepage http://www.liumapp.com
  * @date 5/25/18
  */
-<template>
-</template>
-<script>
-</script>
