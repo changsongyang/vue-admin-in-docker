8f5b4aacdcc67608597d46d8c0196c3a719d8138
liumapp <liumapp.com@gmail.com>
2018-05-25 15:54:06 +0800

    add user components

---
+++ src/views/user/components/tableData.js
@@ -0,0 +1,60 @@
+/**
+ * @file tableData.js
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/25/18
+ */
+export const personalUserTableColumn = [
+    {
+        title: '序号',
+        type: 'index',
+        width: 80,
+        align: 'center'
+    },
+    {
+        title: '用户ID',
+        align: 'center',
+        key: 'userid',
+        width: 80
+    },
+    {
+        title: '昵称',
+        align: 'center',
+        key: 'username'
+    },
+    {
+        title: '邮箱',
+        align: 'center',
+        key: 'email'
+    },
+    {
+        title: '手机号码',
+        align: 'center',
+        key: 'phone'
+    },
+    {
+        title: '状态',
+        align: 'center',
+        render: (h, params) => {
+            const row = params.row;
+            const color = row.status === 0 ? 'red' : row.status === 1 ? 'green' : 'blue';
+            const text = row.status === 0 ? '未生效' : row.status === 1 ? '生效' : '等待注册';
+            return h('Tag', {
+                props: {
+                    type: 'dot',
+                    color: color
+                }
+            }, text);
+        }
+    }
+];
+
+export const companyUserTableColumn = [];
+
+const tableData = {
+    personalUserTableColumn: personalUserTableColumn,
+    companyUserTableColumn: companyUserTableColumn
+};
+
+export default tableData;
