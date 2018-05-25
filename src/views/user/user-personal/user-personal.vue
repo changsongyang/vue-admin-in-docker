b49f14481dcf68117107df4758230aacec774b6c
liumapp <liumapp.com@gmail.com>
2018-05-25 16:06:11 +0800

    add user-personal list

--- src/views/user/user-personal/user-personal.vue
+++ src/views/user/user-personal/user-personal.vue
@@ -6,8 +6,35 @@
  * @date 5/25/18
  */
 <template>
-
 </template>
 <script>
-
+import tableData from '../components/tableData.js';
+import util from '@/libs/util.js';
+import userPersonalTable from '../components/userPersonalTable.vue';
+export default {
+    name: 'user-personal',
+    components: {
+        userPersonalTable
+    },
+    data () {
+        return {
+            personalTableColumn: [],
+            personalTableData: []
+        };
+    },
+    methods: {
+        getData () {
+            this.personalTableColumn = tableData.personalTableColumn;
+            util.post('admin-api/admin/user/list', {
+                type: 0,
+                pageno: 1
+            }).then(res => {
+                this.personalTableData = res.data.content.datalist;
+            });
+        }
+    },
+    created () {
+        this.getData();
+    }
+};
 </script>
