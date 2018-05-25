15ff9d2d4d87a2fefb1dde20085d53324c841640
liumapp <liumapp.com@gmail.com>
2018-05-25 15:56:12 +0800

    ready to add user table

--- src/views/user/components/userCompanyTable.vue
+++ src/views/user/components/userCompanyTable.vue
@@ -1,13 +1,11 @@
 /**
- * @file user-personal.vue
+ * @file userCompanyTable.vue
  * @author liumapp
  * @email liumapp.com@gmail.com
  * @homepage http://www.liumapp.com
  * @date 5/25/18
  */
 <template>
-
 </template>
 <script>
-
 </script>
