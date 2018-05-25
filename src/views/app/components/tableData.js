1038eeab7c2f1d1a27bdcd0e40c443e7103ee89c
liumapp <liumapp.com@gmail.com>
2018-05-25 15:35:13 +0800

    add app components

---
+++ src/views/app/components/tableData.js
@@ -0,0 +1,19 @@
+/**
+ * @file tableData.js
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/25/18
+ */
+export const packageTableColumn = [
+
+];
+
+export const appTableColumn = [];
+
+const tableData = {
+    packageTableColumn: packageTableColumn,
+    appTableColumn: appTableColumn
+};
+
+export default tableData;
