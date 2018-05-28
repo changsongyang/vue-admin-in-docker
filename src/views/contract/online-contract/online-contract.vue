43958b99510fd5ba05a5db8f8a8a3e7d339c67bd
liumapp <liumapp.com@gmail.com>
2018-05-28 14:20:49 +0800

    ready to add online contract table

--- src/views/contract/online-contract/online-contract.vue
+++ src/views/contract/online-contract/online-contract.vue
@@ -1,4 +1,33 @@
+/**
+ * @file online-contract.vue
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/28/18
+ */
 <template>
 </template>
 <script>
+import tableData from '../components/tableData';
+import onlineContractTable from '../components/onlineContractTable.vue';
+export default {
+    name: 'online-contract',
+    components: {
+        onlineContractTable
+    },
+    data () {
+        return {
+            onlineContractColumn: [],
+            onlineContractData: []
+        };
+    },
+    methods: {
+        getData () {
+            this.onlineContractColumn = tableData.onlineContractColumn;
+        }
+    },
+    created () {
+        this.getData();
+    }
+};
 </script>
