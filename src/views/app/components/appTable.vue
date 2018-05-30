e764d67deb66876f60935b09b01cc10e6ca05c49
liumapp <liumapp.com@gmail.com>
2018-05-30 10:28:51 +0800

    update app table

---
+++ src/views/app/components/appTable.vue
@@ -0,0 +1,41 @@
+/**
+ * @file appTable.vue
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/30/18
+ */
+<template>
+</template>
+<script>
+export default {
+    name: 'app-table',
+    props: {
+        refs: String,
+        appTableColumn: Array,
+        value: Array
+    },
+    data () {
+        return {
+            columns: [],
+            appTableData: []
+        };
+    },
+    components: {
+
+    },
+    methods: {
+        init () {
+
+        }
+    },
+    created () {
+        this.init();
+    },
+    watch: {
+        value (data) {
+            this.init();
+        }
+    }
+};
+</script>
