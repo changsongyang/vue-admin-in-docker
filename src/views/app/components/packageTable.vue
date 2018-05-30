66044ea3c3f7e416387d80533d6e001192d36c54
liumapp <liumapp.com@gmail.com>
2018-05-30 10:02:52 +0800

    ready to add package table

---
+++ src/views/app/components/packageTable.vue
@@ -0,0 +1,14 @@
+/**
+ * @file packageTable.vue
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/30/18
+ */
+<template>
+</template>
+<script>
+export default {
+    name: 'package-table'
+};
+</script>
--- src/views/app/custom-package/custom-package.vue
+++ src/views/app/custom-package/custom-package.vue
@@ -8,4 +8,29 @@
 <template>
 </template>
 <script>
+import tableData from '../components/tableData';
+export default {
+    name: 'custom-package',
+    components: {
+
+    },
+    data () {
+        return {
+            packageTableColumn: [],
+            packageTableData: []
+        };
+    },
+    methods: {
+        getData () {
+            this.packageTableColumn = tableData.packageTableColumn;
+            const msg = this.$Message.loading({
+                content: 'Loading....',
+                duration: 0
+            });
+        }
+    },
+    created () {
+        this.getData();
+    }
+};
 </script>
